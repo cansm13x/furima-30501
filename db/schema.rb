5f37d6d666ec61bc8690dc7f9b3310973405f886
cansm13x <cansm.13x@gmail.com>
2020-10-17 20:59:45 +0900

    ユーザー認証機能の実装
    ・マイグレーションファイルのuserターブルのカラム名も変更。
    それに合わせてREADMEも修正。

--- db/schema.rb
+++ db/schema.rb
@@ -19,13 +19,13 @@ ActiveRecord::Schema.define(version: 2020_10_17_071345) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "first_name_full", null: false
-    t.string "last_name_full", null: false
-    t.string "first_name_kana", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
     t.string "last_name_kana", null: false
-    t.date "birthday", null: false
+    t.string "first_name_kana", null: false
+    t.date "birth_date", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
