898c1fec8ca9a5abcc86d296107c16de4015e213
cansm13x <cansm.13x@gmail.com>
2020-10-19 23:07:49 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,8 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category, :status, :delivery, :area, :day
-  
+
+  validates :name, :description, :category, :status, :delivery, :area, :day, :price, presence: true
+
+  validates :category_id, :status_id, :delivery_id, :area_id, :day_id, numericality: { other_than: 1 }
 end
