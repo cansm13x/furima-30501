8bfb42ce3867416cfa7bbe7623b56424a585f012
cansm13x <cansm.13x@gmail.com>
2020-10-19 17:18:17 +0900

    ユーザー管理機能修正２：共通のバリデーションをwith_optionsでまとめる

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :password, presence: true, length: { minimum: 6 }
-  validates :birth_date, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :password,length: { minimum: 6 }
+    validates :birth_date
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
-    validates :last_name, presence: true
-    validates :first_name, presence: true
-    validates :last_name_kana, presence: true
-    validates :first_name_kana, presence: true
-  end
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+      validates :last_name
+      validates :first_name
+      validates :last_name_kana
+      validates :first_name_kana
+    end
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX
 end
