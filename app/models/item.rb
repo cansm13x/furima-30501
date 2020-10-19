bc167f475e27cdb51fab530e24ed029d4441555e
cansm13x <cansm.13x@gmail.com>
2020-10-19 22:23:02 +0900

    ActiveHashを利用するモデルファイルと商品モデルのアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :status, :delivery, :area, :day
+  
 end
