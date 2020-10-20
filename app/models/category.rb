38b801e96a11333b0a320133ca815dfb9e1617b4
cansm13x <cansm.13x@gmail.com>
2020-10-20 14:59:11 +0900

    ActiveHashのモデルファイル修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveHash::Base
-  self.date = [
+  self.data = [
     {id: 1, name: '---'}, {id: 2, name: 'レディース'},
     {id: 3, name: 'メンズ'}, {id: 4, name: 'ベビー・キッズ'},
     {id: 5, name: 'インテリア・住まい・小物'}, {id: 6, name: '本・音楽・ゲーム'},
