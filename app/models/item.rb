cc9043feb369544193b411e50269d25d0cb5505a
cansm13x <cansm.13x@gmail.com>
2020-10-21 13:54:27 +0900

    商品出品機能のモデル単体テストコードの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,18 @@ class Item < ApplicationRecord
   belongs_to_active_hash :area
   belongs_to_active_hash :day
 
-  validates :name, :description, :category, :status, :delivery, :area, :day, :price, presence: true
+  validates :image, :name, :description, :category, :status, :delivery, :area, :day, :price, presence: true
 
   validates :category_id, :status_id, :delivery_id, :area_id, :day_id, numericality: { other_than: 1 }
+
+  # validates :price, numericality: {greather_than_or_equal_to: 300 }
+  # validates :price, numericality: {less_than_or_equal_to: 9999999}
+  validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+
+  def was_attached?
+    self.image.attached?
+  end
+
+  # PRICE_REGEX = /\A(?=.*?[\d])\z/i.freeze
+  # validates_format_of :price, with: PRICE_REGEX
 end
---
+++ spec/models/item_spec.rb
@@ -0,0 +1,79 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+  describe '商品出品' do
+    before do
+      @item = FactoryBot.build(:item)
+    end
+
+    context '商品出品がうまくいくとき' do
+      it 'imageが１枚、name、description、category、status、delivery、area、day、priceが存在すれば投稿できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品出品がうまくいかないとき' do
+      it '商品画像がないと出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it '商品名がないと出品できない' do
+        @item.name = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it '商品の説明がないと出品できない' do
+        @item.description = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      it 'カテゴリーの情報がないと出品できない' do
+        @item.category_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank", "Category is not a number")
+      end
+      it '商品の状態についての情報がないと出品できない' do
+        @item.status_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status can't be blank", "Status is not a number")
+      end
+      it '配送料の負担についての情報がないと出品できない' do
+        @item.delivery_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery can't be blank", "Delivery is not a number")
+      end
+      it '発送元の地域についての情報がないと出品できない' do
+        @item.area_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Area can't be blank", "Area is not a number")
+      end
+      it '発送までの日数についての情報がないと出品できない' do
+        @item.day_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Day can't be blank", "Day is not a number")
+      end
+      it '価格についての情報がないと出品できない' do
+        @item.price = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it '販売価格は300円以下だと出品できない' do
+        @item.price = "299"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it '販売価格は9999999円以上だと出品できない' do
+        @item.price = "10000000"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+
+      end
+      it '販売価格は半角数字以外だと保存できない' do
+        @item.price = "１０００"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+    end
+  end
+end
