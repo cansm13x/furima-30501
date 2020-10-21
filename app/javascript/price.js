bf12ee69c3d3bde33700976a94e75105f16cefd1
cansm13x <cansm.13x@gmail.com>
2020-10-21 14:50:21 +0900

    入力した金額の取得

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,7 +1,8 @@
 window.addEventListener('load', () =>{
   const priceInput = document.getElementById("price");
   priceInput.addEventListener("input", () => {
-    console.log("イベント発火");
+    const inputValue = priceInput.value;
+    console.log(inputValue);
   })
   const priceTax = document.getElementById("add-tax-price");
   priceTax.addEventListener("input", () => {
