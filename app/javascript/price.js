34f895098760b8244c15aa585158198eceae0a6c
cansm13x <cansm.13x@gmail.com>
2020-10-21 14:47:50 +0900

    金額を入力するとイベント発火できる

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,8 +1,14 @@
 window.addEventListener('load', () =>{
   const priceInput = document.getElementById("price");
-  console.log(priceInput);
+  priceInput.addEventListener("input", () => {
+    console.log("イベント発火");
+  })
   const priceTax = document.getElementById("add-tax-price");
-  console.log(priceTax)
+  priceTax.addEventListener("input", () => {
+    console.log(priceTax);
+  })
   const priceProfit = document.getElementById("profit");
-  console.log(priceProfit)
+  priceProfit.addEventListener("input", () => {
+    console.log(priceProfit);
+  })
 })
