39a47c94754810798a923721f60bfcc3bab542b5
cansm13x <cansm.13x@gmail.com>
2020-10-21 17:49:27 +0900

    商品出品ページ表示の実装修正１：不要のコメントアウト削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,14 +13,9 @@ class Item < ApplicationRecord
 
   validates :category_id, :status_id, :delivery_id, :area_id, :day_id, numericality: { other_than: 1 }
 
-  # validates :price, numericality: {greather_than_or_equal_to: 300 }
-  # validates :price, numericality: {less_than_or_equal_to: 9999999}
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   def was_attached?
     image.attached?
   end
-
-  # PRICE_REGEX = /\A(?=.*?[\d])\z/i.freeze
-  # validates_format_of :price, with: PRICE_REGEX
 end
