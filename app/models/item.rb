2ed43a4f96faf509da1dca8bb44c06789a801597
cansm13x <cansm.13x@gmail.com>
2020-10-21 17:51:39 +0900

    商品出品ページ表示の実装修正１：priceのpresence

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,11 +9,11 @@ class Item < ApplicationRecord
   belongs_to_active_hash :area
   belongs_to_active_hash :day
 
-  validates :image, :name, :description, :category, :status, :delivery, :area, :day, :price, presence: true
+  validates :image, :name, :description, :category, :status, :delivery, :area, :day, presence: true
 
   validates :category_id, :status_id, :delivery_id, :area_id, :day_id, numericality: { other_than: 1 }
 
-  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   def was_attached?
     image.attached?
