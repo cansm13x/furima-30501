e309eb3bb0dc13af21354aaf06f4a4a4f4b2b7ff
cansm13x <cansm.13x@gmail.com>
2020-10-21 18:05:43 +0900

    商品出品ページ表示の実装修正１：(')は不要の為削除

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -84,12 +84,12 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
       it '販売価格は300円以下だと出品できない' do
-        @item.price = '299'
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
       it '販売価格は9999999円以上だと出品できない' do
-        @item.price = '10000000'
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
