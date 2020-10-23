700e0cf80d88bfcdec872970973337df908c6232
cansm13x <cansm.13x@gmail.com>
2020-10-23 17:33:45 +0900

    before_actionで一つにまとめる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_item, only: [:show, :edit]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -19,11 +20,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
@@ -39,5 +38,9 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :name, :category_id, :description, :status_id, :delivery_id, :area_id, :day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+     @item = Item.find(params[:id])
   end
 end
