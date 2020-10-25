a84257b65b3470431fb0919ab52ed112341ca096
cansm13x <cansm.13x@gmail.com>
2020-10-25 16:13:18 +0900

    購入機能に必要なモデルやコントローラーの作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,21 @@
 class OrdersController < ApplicationController
-  def show
-    @order = Order.find(params[:id])
+  def index
+    @user_buy = UserBuy.new
+  end
+
+  def create
+    @user_buy = UserBuy.new(order_params)
+    if @user_buy.valid?
+      @user_buy.save
+      redirect_to root_path
+    else 
+      render action: :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:user_buy).permit(:postal_code, :prefecture_id, :municipality, :addres, :building_name, :tell, :order_user_id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_many :order_users
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
