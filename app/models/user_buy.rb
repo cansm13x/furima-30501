a67efb569d49b1d710f998867a60a467a88211f5
cansm13x <cansm.13x@gmail.com>
2020-10-26 19:00:30 +0900

    フォームの情報を保存できるよう修正

--- app/models/user_buy.rb
+++ app/models/user_buy.rb
@@ -1,7 +1,8 @@
 class UserBuy
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :municipality, 
-                :addres, :building_name, :tell, :order_user_id
+                :addres, :building_name, :tell, :order_user_id,
+                :user_id, :item_id
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
@@ -14,7 +15,7 @@ class UserBuy
   validates :prefecture_id, numericality: { other_than: 0 }
 
   def save
-    order_user = Order_users.create(user_id: user_id, item_id: item_id)
-    Order.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, addres: addres, building_name: building_name, tell: tell, order_user_id: order_user.id)
+    order_user_id = OrderUser.create(user_id: user_id, item_id: item_id)
+    Order.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, addres: addres, building_name: building_name, tell: tell, order_user_id: order_user_id.id)
   end
 end
