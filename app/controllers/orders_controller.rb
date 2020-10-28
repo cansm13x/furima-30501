5dde02d52329048b25d99411e32ddf79574cba4b
cansm13x <cansm.13x@gmail.com>
2020-10-28 17:06:26 +0900

    決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,10 +5,15 @@ class OrdersController < ApplicationController
   end
 
   def create
-    binding.pry
     @item = Item.find(params[:item_id])
     @user_buy = UserBuy.new(order_params)
     if @user_buy.valid?
+      Payjp.api_key = "sk_test_82b63b2bef8f4396fc6d6b43"
+      Payjp::Charge.create( 
+        amount: @item[:price],
+        card: order_params[:token],
+        currency: 'jpy'
+      )
       @user_buy.save
       redirect_to root_path
     else 
@@ -19,6 +24,6 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:user_buy).permit(:postal_code, :prefecture_id, :municipality, :addres, :building_name, :tell, :order_user_id).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:user_buy).permit(:postal_code, :prefecture_id, :municipality, :addres, :building_name, :tell, :order_user_id).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 end
--- app/models/user_buy.rb
+++ app/models/user_buy.rb
@@ -2,7 +2,7 @@ class UserBuy
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :municipality, 
                 :addres, :building_name, :tell, :order_user_id,
-                :user_id, :item_id
+                :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
@@ -10,7 +10,6 @@ class UserBuy
     validates :municipality, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
     validates :addres, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
     validates :tell, format: { with: /\A\d{11}\z/ } 
-
   end
 
   validates :prefecture_id, numericality: { other_than: 0 }
